9771524c5cbfb8aae9eedefdf188ec08420709de
lulultfh <punyalulu05@gmail.com>
2024-03-07 18:12:34 +0700

    membuat prosedur proses BubbleSort

--- TugasPertemuanKetiga/TugasPertemuanKetiga.cpp
+++ TugasPertemuanKetiga/TugasPertemuanKetiga.cpp
@@ -29,3 +29,21 @@ void input() {
 	}
 }
 
+void BubbleSortArray() {//procedur untuk mengurutkan array dengan metode bubble sort
+	int pass = 1; //step 1
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) {//step 2
+			if (arr[j] > arr[j + 1]) {//step 3
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+
+			}
+		}
+
+		pass = pass + 1; //step 4
+	} while (pass <= n); //step 5
+
+}
+
